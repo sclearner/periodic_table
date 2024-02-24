f8855134f43128e3130b20a2918c24a6a1c2111d
Sam Ngo Ngoc <ngocsam11110010@gmail.com>
2024-02-24 10:52:24 +0000

    feat: search element

--- element.sh
+++ element.sh
@@ -5,5 +5,17 @@ then
   echo "Please provide an element as an argument."
 else
   PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-  
+  # columns need to get
+  COLUMNS="atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius"
+  # check atomic number
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    RESULT=$($PSQL "SELECT $COLUMNS FROM elements inner join properties using(atomic_number) inner join types using(type_id) where atomic_number=$1")
+  else
+    RESULT=$($PSQL "SELECT $COLUMNS FROM elements inner join properties using(atomic_number) inner join types using(type_id) where name='$1'")
+    if [[ -z $RESULT ]]
+    then
+      RESULT=$($PSQL "SELECT $COLUMNS FROM elements inner join properties using(atomic_number) inner join types using(type_id) where symbol='$1'")  
+    fi
+  fi
 fi
