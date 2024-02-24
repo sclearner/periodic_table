dfe31c6399f1b06fed4655484262c1aa381ae4ad
Sam Ngo Ngoc <ngocsam11110010@gmail.com>
2024-02-24 10:56:03 +0000

    feat: return info

--- element.sh
+++ element.sh
@@ -18,4 +18,12 @@ else
       RESULT=$($PSQL "SELECT $COLUMNS FROM elements inner join properties using(atomic_number) inner join types using(type_id) where symbol='$1'")  
     fi
   fi
+  # return result
+  if [[ $RESULT ]]
+  then
+    echo $RESULT | read atomic_number bar name bar symbol bar type bar atomic_mass bar melting_point_celsius bar boiling_point_celsius
+    echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. Hydrogen has a melting point of $melting_point_celsius celsius and a boiling point of $boiling_point_celsius celsius."
+  else
+    echo "I could not find that element in the database."
+  fi
 fi
